c480d4a726d6cf6dcd1b8f2e16f935b9a2e1e7c0
Zoriana Cherevko <alisgood05@gmail.com>
2021-11-05 11:10:52 +0200

    change driver

--- src/test/java/page/object/tests/BaseTest.java
+++ src/test/java/page/object/tests/BaseTest.java
@@ -8,29 +8,24 @@ import org.testng.annotations.AfterMethod;
 
 import org.testng.annotations.BeforeSuite;
 
-
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 
 public class BaseTest {
-
-
-    WebDriver driver;
 
     private static final String URL ="http://automationpractice.com/index.php";
 
     @BeforeSuite
     public void profileSetUp(){
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        //Configuration.startMaximized = true;
+        Configuration.startMaximized = true;
         open(URL);
     }
 
-    //@AfterMethod
-   // public void tearDown(){
-       /// closeWebDriver();
-    //}
+    @AfterMethod
+    public void tearDown(){
+        closeWebDriver();
+    }
 
 
 
