a1305b707917aecdad7245b1e3c72d97b664e318
Zoriana Cherevko <alisgood05@gmail.com>
2021-11-07 15:42:32 +0200

    add order endpoints

--- src/main/java/consts/Endpoints.java
+++ src/main/java/consts/Endpoints.java
@@ -6,4 +6,9 @@ public class Endpoints {
     public static final String DELETE_A_PET = "/v2/pet/{id}";
     public static final String CREATE_A_PET = "/v2/pet";
     public static final String UPDATE_A_PET = "/v2/pet";
+
+    public static final String CREATE_AN_ORDER = "/v2/store/order";
+    public static final String GET_AN_ORDER = "/v2/store/order/{orderId}";
+    public static final String DELETE_AN_ORDER = "/v2/store/order/{orderId}";
+
 }
