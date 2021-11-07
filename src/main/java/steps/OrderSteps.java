78d27bff398f54f1720db3383d6d0fc601c8c54c
Zoriana Cherevko <alisgood05@gmail.com>
2021-11-07 15:59:35 +0200

    add order Steps

---
+++ src/main/java/steps/OrderSteps.java
@@ -0,0 +1,29 @@
+package steps;
+
+import builders.Order;
+import builders.OrderStatus;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import lombok.SneakyThrows;
+import utils.ApiUtils;
+import utils.BuilderUtil;
+import static consts.Endpoints.*;
+
+import java.util.Date;
+
+public class OrderSteps {
+
+    private final BuilderUtil builderUtil = new BuilderUtil();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @SneakyThrows
+    @Step("Add a new order to store")
+    public Response addNewOrderToStore(int orderId, int petId, int quantity, Date shipDate,
+                                       OrderStatus orderStatus, boolean complete) {
+        Order order = builderUtil.buildOrder(orderId, petId, quantity, shipDate, orderStatus, complete);
+
+        return ApiUtils.post(CREATE_AN_ORDER, objectMapper.writeValueAsString(order));
+
+    }
+}
