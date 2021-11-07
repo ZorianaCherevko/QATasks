cc06e22600576f3171394ae20b88a212a36d355a
Zoriana Cherevko <alisgood05@gmail.com>
2021-11-07 16:12:51 +0200

    add test cred for an order

--- src/main/java/test/Cred/TestCred.java
+++ src/main/java/test/Cred/TestCred.java
@@ -1,5 +1,9 @@
 package test.Cred;
+import builders.OrderStatus;
 import builders.Status;
+
+import java.util.Date;
+
 public class TestCred {
 
     public static int PET_CATEGORY_ID = 2;
@@ -11,6 +15,13 @@ public class TestCred {
     public static int PET_TAG_ID = 13;
     public static String PET_TAG_NAME = "Hound";
     public static Status UPDATED_PET_STATUS = Status.sold;
+
+
+    public static int ORDER_ID = 25;
+    public static int ORDER_QUANTITY = 3;
+    public static Date ORDER_SHIP_DATE = new Date();
+    public static OrderStatus ORDER_STATUS = OrderStatus.placed;
+    public static boolean complete;
 
 
 }
